3c4ba91597097fe4ad9c399c99e8c6ce37470777
Daniel De Aguiar <ddeaguiar@pointslope.com>
2009-05-23 19:39:22 -0400

    added sim_pearson, top_matches

--- similarity.rb
+++ similarity.rb
@@ -24,3 +24,38 @@ def sim_distance(prefs, person1, person2)
   end
   1/(1 + Math.sqrt(sum_of_square_diffs))
 end
+
+#page 13
+def sim_pearson(prefs, person1, person2)
+  si = prefs[person1].keys & prefs[person2].keys
+  n = si.size
+  return 0 if n == 0
+  
+  sum1 = 0
+  sum2 = 0
+  sum1sq = 0
+  sum2sq = 0
+  psum = 0
+  si.each do |key|
+    sum1 += prefs[person1][key]
+    sum2 += prefs[person2][key]
+    sum1sq += sqr(prefs[person1][key])
+    sum2sq += sqr(prefs[person2][key])
+    psum += prefs[person1][key] * prefs[person2][key]
+  end
+  
+  num = psum - (sum1 * sum2 / n)
+  #Corrects the typo in the First edition printing which forgets
+  #the sqrt.
+  den = Math.sqrt((sum1sq - sqr(sum1) / n) * (sum2sq - sqr(sum2) / n))
+  
+  den == 0 ? 0 : num/den
+end
+
+#page 14
+def top_matches(prefs, person, n=5, similarity=:sim_pearson)
+  scores = (prefs.keys - [person]).collect do |other|
+              [send(similarity, prefs, person, other), other]
+            end
+  scores.sort().reverse().slice(0..n-1)
+end
